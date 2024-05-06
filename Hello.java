5cf4cb5dbbc80b0fe525a5fe6d76ffbd2fc0c0f7
LAMI <tolboni614@gmail.com>
2024-05-06 07:09:04 -0700

    commit2

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
     public static void main(String[] args) {
         System.out.println("Hello world, I returned for coding java");
+        System.out.println("HI how is code");
     }
 }
