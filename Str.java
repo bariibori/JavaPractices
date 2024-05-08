8150dc3de510c489acf074d6b50ff01d6d941b6e
LAMI <tolboni614@gmail.com>
2024-05-08 01:04:20 -0700

    prints a formatted string

---
+++ Str.java
@@ -0,0 +1,10 @@
+public class Str {
+    public static void main(String[] args) {
+        String name ="Boni";
+        int age =22;
+        double height =1.88;
+        System.out.printf("Name: %s, Age : %d, Height :%.2f",name,age,height);
+    }
+}
+    
+    
