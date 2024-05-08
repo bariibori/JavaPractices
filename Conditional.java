e227d60c349061a3fbff6b082354bd190f195571
LAMI <tolboni614@gmail.com>
2024-05-08 01:49:43 -0700

    Conditional

---
+++ Conditional.java
@@ -0,0 +1,11 @@
+public class Conditional {
+   public static void main(String[] args) {
+    int age =18;
+    if (age >= 18) {
+        System.out.println("You are an adult.👮‍♂️");
+    } else {
+        System.out.println("You are not an adult.👨");
+    }   
+   }
+    
+}
