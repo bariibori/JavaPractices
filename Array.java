e5d1f8546326bba4061912d0c00fdd6a83ef16c2
LAMI <tolboni614@gmail.com>
2024-05-11 11:11:42 +0300

    Printing array using for-each loop

---
+++ Array.java
@@ -0,0 +1,8 @@
+public class Array {
+    public static void main(String[] args) {
+      int  arr[]={20,30,12,22,43,76};
+      for (int i : arr) {
+        System.out.println(i);
+      }
+    }
+}
