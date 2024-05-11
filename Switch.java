279147ff26191a468775e9e58f56bda24a9eb9e1
LAMI <tolboni614@gmail.com>
2024-05-11 10:30:25 +0300

    switch condition

---
+++ Switch.java
@@ -0,0 +1,11 @@
+public class Switch{
+    public static void main(String[] args) {
+        int num =20;
+        switch (num) {
+            case 10 -> System.out.println("10");
+            case 20 -> System.out.println("20");
+             case 30 -> System.out.println(30);
+            default -> System.out.println("Not in 10,20,30");
+        }
+    }
+}
