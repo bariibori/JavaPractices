1515ca14e8b8c1ed3cccb3a009afdc839419023c
LAMI <tolboni614@gmail.com>
2024-05-11 17:10:38 +0300

    Input From User inJava

--- Sum.java
+++ Sum.java
@@ -1,15 +1,15 @@
 import java.util.*;
 public class Sum {
     public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Frist Number: ");
-        double x= sc.nextDouble();
-        System.out.print("Enter Second Number: ");
-        double y=sc.nextDouble();
-        System.out.print("Enter Third Number: ");
-        double z =sc.nextDouble();
-        double sum=x+y+z;
-        System.out.println("The SUM of Three Number is = "+sum);
-      
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter Frist Number: ");
+            double x= sc.nextDouble();
+            System.out.print("Enter Second Number: ");
+            double y=sc.nextDouble();
+            System.out.print("Enter Third Number: ");
+            double z =sc.nextDouble();
+            double sum=x+y+z;
+            System.out.println("The SUM of Three Number is = "+sum);
+        }
     }
 }
